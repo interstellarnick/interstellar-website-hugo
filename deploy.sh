0081941a50492bdf3ba197ec3dcd2c190510eabc
neodigm <3151842+neodigm@users.noreply.github.com>
2023-11-04 21:25:43 -0500

    lottie

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ cd ../interstellar-website
 git status
 git pull
 cd ../interstellar-website-hugo
+cp -r static/* public/
 cp -r public/* ../interstellar-website
 cd ../interstellar-website
 git status
